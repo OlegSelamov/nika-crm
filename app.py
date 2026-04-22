b43edf78ba30aba708f0aefc8735c8eb2c633e1b
Oleg Shelamov <olegselamov15@gmail.com>
2026-04-22 23:38:59 +0500

    fix double run and port for render

--- app.py
+++ app.py
@@ -75,7 +75,5 @@ def check_company_access():
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
-    
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
