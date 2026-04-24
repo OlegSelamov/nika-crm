6ab6e03c1d66efe49b04fcfbb32a74ca6bd5bd0c
Oleg Shelamov <olegselamov15@gmail.com>
2026-04-24 22:38:33 +0500

    fix database path to persistent disk

--- models.py
+++ models.py
@@ -1,10 +1,14 @@
+import os
 import sqlite3
 from datetime import datetime
 
-DATABASE = "database.db"
+DATABASE = "/data/database.db"
+
+if not os.path.exists("/data"):
+    os.makedirs("/data")
 
 def get_db():
-    conn = sqlite3.connect("database.db", timeout=5)
+    conn = sqlite3.connect(DATABASE, timeout=5)
     conn.row_factory = sqlite3.Row
     return conn
 
