e183502b9d63608725fb65ed9bee5df3ebcae5b0
Oleg Shelamov <olegselamov15@gmail.com>
2026-08-26 12:09:44 +0500

    Создание Интеграций5

--- test_addin.py
+++
@@ -1,8 +0,0 @@
-import clr
-
-clr.AddReference(r"C:\Program Files (x86)\Newton\Драйвер ККМ\Примеры\C#\WinPortTest\obj\x86\Debug\Interop.AddInLib.dll")
-
-import AddInLib
-
-print("Содержимое AddInLib:")
-print(dir(AddInLib))
