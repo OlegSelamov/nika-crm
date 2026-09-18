fe4bd4127f8f2608e3097938186ac49de22aeeb6
OlegSelamov <olegselamov15@gmail.com>
2026-09-19 04:05:03 +0500

    Package official Kalkan SDK libraries from Android app libs

--- android_app/android/app/build.gradle.kts
+++ android_app/android/app/build.gradle.kts
@@ -61,6 +61,13 @@ android {
     }
 }
 
+dependencies {
+    implementation(fileTree(mapOf(
+        "dir" to "libs",
+        "include" to listOf("*.jar", "*.aar")
+    )))
+}
+
 flutter {
     source = "../.."
 }
