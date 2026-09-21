0b94d157b8216507f24b82d10b33fd8a4a12bcc4
OlegSelamov <olegselamov15@gmail.com>
2026-09-21 23:51:31 +0500

    Refresh measurement unit UI

--- static/js/items.js
+++ static/js/items.js
@@ -1,4 +1,4 @@
 (function(){
-  document.write('<scr'+'ipt charset="UTF-8" src="/static/js/items_legacy.js?v=20260921-business-mode-2"></scr'+'ipt>');
+  document.write('<scr'+'ipt charset="UTF-8" src="/static/js/items_legacy.js?v=20260921-measure-units-1"></scr'+'ipt>');
   document.write('<scr'+'ipt charset="UTF-8" src="/static/js/catalog_price_lock.js?v=20260917-1"></scr'+'ipt>');
 })();
