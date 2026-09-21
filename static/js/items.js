74378673c0f36d614eea8a91a04b971b6836b89c
OlegSelamov <olegselamov15@gmail.com>
2026-09-22 00:04:00 +0500

    Refresh mobile unit filtering

--- static/js/items.js
+++ static/js/items.js
@@ -1,4 +1,4 @@
 (function(){
-  document.write('<scr'+'ipt charset="UTF-8" src="/static/js/items_legacy.js?v=20260921-measure-units-2"></scr'+'ipt>');
+  document.write('<scr'+'ipt charset="UTF-8" src="/static/js/items_legacy.js?v=20260922-measure-units-mobile"></scr'+'ipt>');
   document.write('<scr'+'ipt charset="UTF-8" src="/static/js/catalog_price_lock.js?v=20260917-1"></scr'+'ipt>');
 })();
